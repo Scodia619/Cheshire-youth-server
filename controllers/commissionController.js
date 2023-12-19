1fee07fd27a44ea994039f8b56fd7c7a115c9758
Will <billyjoe2701@gmail.com>
2023-12-19 03:00:09 +0000

    Working on selectCommissionByName and Get reports by commission

--- controllers/commissionController.js
+++ controllers/commissionController.js
@@ -1,6 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+//Reject on Not Found Error
 exports.selectCommissionByName = async (req, res, next) => {
     const {commission_name} = req.params
     try{
